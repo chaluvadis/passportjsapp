d2d393c2478cebc005dafd4477c8094632421790
chaluvadis <Surendra@Surendra.local>
2016-10-31 22:01:39 +0400

    facebook login issue is fixed

--- models/auth.js
+++ models/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebook' : {
         clientID: '336263763392641',
         clientSecret: '322e00e0df9fef2ad6e98fc73e04de40',
-        callbackURL : 'http://localhost:1337/auth/facebook/callback'
+        callbackURL : 'http://localhost:1337/users/auth/facebook/callback'
     }
 }
